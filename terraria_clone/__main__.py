aca3d23a4e9eddb37ef1fcb7d1241bca7e2c5cfc
Fausto A. Guerrero <enjoythelive1@gmail.com>
2020-10-18 19:42:24 +0200

    Splits the ghost and the world (ghost land)

--- terraria_clone/__main__.py
+++ terraria_clone/__main__.py
@@ -2,7 +2,27 @@ import cocos
 
 from terraria_clone.agents.ghost import Ghost
 
+import pyglet
+import cocos
+
+
+class GhostLand(cocos.layer.Layer):
+    is_event_handler = True
+
+    def __init__(self):
+        super(GhostLand, self).__init__()
+        self.agent = Ghost()
+        self.agent.position = 320, 240
+        self.add(self.agent)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.agent.move_to(x, y)
+
+    def on_key_press (self, key, modifiers):
+        if (key == pyglet.window.key.SPACE):
+            self.agent.rotate()
+
 
 cocos.director.director.init()
-main_scene = cocos.scene.Scene(Ghost())
+main_scene = cocos.scene.Scene(GhostLand())
 cocos.director.director.run(main_scene)
--- terraria_clone/agents/ghost.py
+++ terraria_clone/agents/ghost.py
@@ -1,20 +1,13 @@
-import pyglet
 import cocos
 
 
-class Ghost(cocos.layer.Layer):
-    is_event_handler = True
-
+class Ghost(cocos.sprite.Sprite):
     def __init__(self):
-        super(Ghost, self).__init__()
-        self.sprite = cocos.sprite.Sprite('assets/ghost_idle_1.png')
-        self.sprite.scale = 0.1
-        self.add(self.sprite)
-        self.position = 320, 240
+        super(Ghost, self).__init__('assets/ghost_idle_1.png')
+        self.scale = 0.1
 
-    def on_mouse_motion(self, x, y, dx, dy):
+    def move_to(self, x, y):
         self.position = x, y
 
-    def on_key_press (self, key, modifiers):
-        if (key == pyglet.window.key.SPACE):
-            self.sprite.do(cocos.actions.RotateBy(90, 0.2))
+    def rotate(self):
+        self.do(cocos.actions.RotateBy(90, 0.2))
