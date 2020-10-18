e0f2b55bc1e797ccecd6f1ab5967289799d0a491
Angel Velez <angel.guerrero001@hotmail.com>
2020-10-18 09:24:53 -0400

    Crear una ventana con texto

--- terraria-clone/__main__.py
+++ terraria-clone/__main__.py
@@ -1,1 +1,23 @@
-print('Hello world!')
+import cocos
+
+
+class HelloWorld(cocos.layer.Layer):
+    def __init__(self):         
+        super(HelloWorld, self).__init__()
+        
+        label = cocos.text.Label(
+            'fgggggggggggggggggf',
+            font_name='Wide Latin',
+            font_size=20,
+            anchor_x='center', anchor_y='center',
+            color=(255, 255, 255, 255))
+        label.position = 320, 240
+
+        self.add(label)
+
+
+
+cocos.director.director.init()
+hello_layer = HelloWorld()
+main_scene = cocos.scene.Scene(hello_layer)
+cocos.director.director.run(main_scene)
