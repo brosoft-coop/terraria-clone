8f894eeab8dcc32465f4856acb1c275f164719a8
Fausto A. Guerrero <enjoythelive1@gmail.com>
2020-10-18 19:07:12 +0200

    Makes ghost the only item in existance

--- terraria_clone/__main__.py
+++ terraria_clone/__main__.py
@@ -1,47 +1,8 @@
 import cocos
 
-from terraria_clone.labels.hello_label import HelloLabel
 from terraria_clone.agents.ghost import Ghost
 
 
-class HelloWorld(cocos.layer.ColorLayer):
-    def __init__(self):         
-        super(HelloWorld, self).__init__(241, 247, 54, 255)
-        
-        label = HelloLabel()
-        
-        label.position = 320, 240
-
-        self.add(label)
-
-        ghost = Ghost()
-        ghost.position = 470, 240
-
-        self.add(ghost, z=1)
-
-        scale = cocos.actions.ScaleBy(3, duration=0.5)
-
-        label.do(cocos.actions.Repeat(
-            scale
-            + cocos.actions.Reverse(scale)
-        ))
-
-        ghost.do(cocos.actions.Repeat(
-            cocos.actions.Reverse(scale)
-            + scale
-            + (
-                cocos.actions.RotateBy(45, 0.1)
-                | cocos.actions.MoveBy((-200, 3), 0.1)
-            )
-            + cocos.actions.MoveBy((200, -3), .25)
-        ))
-
-
-
 cocos.director.director.init()
-hello_layer = HelloWorld()
-hello_layer.do(cocos.actions.Repeat(
-    cocos.actions.RotateBy(360, duration=2.5)
-))
-main_scene = cocos.scene.Scene(hello_layer)
+main_scene = cocos.scene.Scene(Ghost())
 cocos.director.director.run(main_scene)
