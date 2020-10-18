4912f8734e8ab3cbe70a8f4e79633c703731cb7e
Angel Velez <angel.guerrero001@hotmail.com>
2020-10-18 15:49:33 -0400

    add  Background of ghost

--- terraria_clone/__main__.py
+++ terraria_clone/__main__.py
@@ -11,6 +11,12 @@ class GhostLand(cocos.layer.Layer):
 
     def __init__(self):
         super(GhostLand, self).__init__()
+                
+        background = cocos.sprite.Sprite ('assets/cyperpunk.png')
+        self.add(background)
+        background.position = 320, 240
+        background.scale = 0.45
+
         self.agent = Ghost()
         self.agent.position = 320, 240
         self.add(self.agent)
@@ -21,7 +27,7 @@ class GhostLand(cocos.layer.Layer):
     def on_key_press (self, key, modifiers):
         if (key == pyglet.window.key.SPACE):
             self.agent.rotate()
-
+ 
 
 cocos.director.director.init()
 main_scene = cocos.scene.Scene(GhostLand())
