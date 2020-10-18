14e67dc7b9ba6559f376bf9e1f79ab2c97d0dd80
Fausto A. Guerrero <enjoythelive1@gmail.com>
2020-10-18 18:16:42 +0200

    Mueve y rota todo de manera indefinida

--- terraria-clone/__main__.py
+++ terraria-clone/__main__.py
@@ -24,9 +24,29 @@ class HelloWorld(cocos.layer.ColorLayer):
 
         self.add(ghost, z=1)
 
+        scale = cocos.actions.ScaleBy(3, duration=0.5)
+
+        label.do(cocos.actions.Repeat(
+            scale
+            + cocos.actions.Reverse(scale)
+        ))
+
+        ghost.do(cocos.actions.Repeat(
+            cocos.actions.Reverse(scale)
+            + scale
+            + (
+                cocos.actions.RotateBy(45, 0.1)
+                | cocos.actions.MoveBy((-200, 3), 0.1)
+            )
+            + cocos.actions.MoveBy((200, -3), .25)
+        ))
+
 
 
 cocos.director.director.init()
 hello_layer = HelloWorld()
+hello_layer.do(cocos.actions.Repeat(
+    cocos.actions.RotateBy(360, duration=2.5)
+))
 main_scene = cocos.scene.Scene(hello_layer)
 cocos.director.director.run(main_scene)
