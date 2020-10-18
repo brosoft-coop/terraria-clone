b31538299978335d7edc315ff4cfad4919db838e
Fausto A. Guerrero <enjoythelive1@gmail.com>
2020-10-18 19:16:11 +0200

    Makes the ghost move with the mouse

--- terraria_clone/agents/ghost.py
+++ terraria_clone/agents/ghost.py
@@ -1,7 +1,15 @@
 import cocos
 
 
-class Ghost(cocos.sprite.Sprite):
+class Ghost(cocos.layer.Layer):
+    is_event_handler = True
+
     def __init__(self):
-        super(Ghost, self).__init__('assets/ghost_idle_1.png')
-        self.scale = 0.5
+        super(Ghost, self).__init__()
+        self.sprite = cocos.sprite.Sprite('assets/ghost_idle_1.png')
+        self.sprite.scale = 0.1
+        self.add(self.sprite)
+        self.position = 320, 240
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.position = x, y
